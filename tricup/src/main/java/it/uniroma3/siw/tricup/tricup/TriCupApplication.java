cd2fc02d4fbea7c3e954e529469ba647cb9df332
SULLIVANG50-70 <lguidaldi@outlook.com>
2017-06-06 16:37:54 +0200

    creato progetto maven con spring initializr

---
+++ tricup/src/main/java/it/uniroma3/siw/tricup/tricup/TriCupApplication.java
@@ -0,0 +1,12 @@
+package it.uniroma3.siw.tricup.tricup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriCupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriCupApplication.class, args);
+	}
+}
---
+++ tricup/src/test/java/it/uniroma3/siw/tricup/tricup/TriCupApplicationTests.java
@@ -0,0 +1,16 @@
+package it.uniroma3.siw.tricup.tricup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TriCupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
